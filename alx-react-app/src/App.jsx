0fdc2a1489e8916c805f703fc4b52e46eb5d70e4
kingfetson <171272139+kingfetson@users.noreply.github.com>
2025-07-08 11:56:34 +0300

    Add Header, MainContent, and Footer components with basic CSS styling

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
+import './App.css';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
- return (
+  return (
     <div>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
