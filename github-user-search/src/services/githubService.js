9fb7f2ad2876038d4703fb1a9c3830164f9f692f
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2025-07-29 23:53:32 +0300

    updated githubservice file

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_APP_GITHUB_API_URL || "https://api.github.com";
 
-// Advanced Search
+/**
+ * Advanced User Search
+ * Endpoint: https://api.github.com/search/users?q=
+ */
 export const advancedUserSearch = async (username, location, minRepos) => {
   try {
     let query = "";
