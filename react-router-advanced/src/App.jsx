8855a04b6cf05c5c628ecf7ac35db2cb82e98cf2
kingfetson <171272139+kingfetson@users.noreply.github.com>
2025-08-21 21:53:20 +0300

    fix: profile.jsx moved to components

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Profile from "./pages/Profile";
+import Profile from "./components/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
